71a9d9fbf0850fa3c6adf36cb60da4210f791bad
Daemian Koh <Daemian_Koh@singaporeair.com.sg>
2023-04-12 10:28:53 +0800

    add java code

---
+++ idea/src/test/java/sia/hackathon/idea/IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.hackathon.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
