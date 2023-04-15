1b284991b8c42797db02ae9f200e8bc28a984dca
Daemian Koh <Daemian_Koh@singaporeair.com.sg>
2023-04-15 11:20:49 +0800

    test load ui

--- idea/src/main/java/sia/hackathon/idea/controller/IdeaController.java
+++ idea/src/main/java/sia/hackathon/idea/controller/IdeaController.java
@@ -4,8 +4,12 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import sia.hackathon.idea.model.FlightBooking;
 import sia.hackathon.idea.service.IdeaService;
@@ -28,4 +32,9 @@ public class IdeaController {
     public List<FlightBooking> testRepo() throws IOException {
         return ideaService.testRepo();
     }
+	
+	@RequestMapping(value = "/testUI", method = RequestMethod.GET)
+	public ModelAndView detail(String applRefNo, ModelMap map) throws Exception {
+		return new ModelAndView("fragments/index");
+	}
 }
