c3337318a6bb62374909c62a99d3204abd4ddfda
Daemian Koh <Daemian_Koh@singaporeair.com.sg>
2023-04-16 02:08:21 +0800

    populate profile

--- idea/src/main/resources/static/js/detail.js
+++ idea/src/main/resources/static/js/detail.js
@@ -11,8 +11,6 @@ function getDetail() {
 		success: function(data) {
 			if (data.returnCode == 0) {
 				loadSummary(data.o);
-				const contextPath = window.location.pathname.split('/')[2];
-				console.log('Context path:', contextPath);
 			}
 			else {
 				console.log("no record");
@@ -28,5 +26,28 @@ function loadSummary(data) {
 	caluclateLeftOverDate(data.departureFromSgDate);
 	groupOrIndividual(data.numberOfPerson);
 	populateTravelDetail(data);
-	//populateProfiles(data.personInfos);
+	populateRequirement(data.personInfos[0]);
+}
+
+function populateRequirement(personInfo) {
+	
+	document.getElementById("name").textContent = personInfo.name;
+	
+	if (personInfo.visa != 'N') {
+		document.getElementById('cbVisa').checked = true;
+		document.getElementById('cbVisa').disabled = true;
+	}
+	
+	if (personInfo.passport != 'N') {
+		document.getElementById('cbPassport').checked = true;
+		document.getElementById('cbPassport').disabled = true;
+	}
+	if (personInfo.pcr != 'N') {
+		document.getElementById('cbPcr').checked = true;
+		document.getElementById('cbPcr').disabled = true;
+	}
+	if (personInfo.vaccination != 'N') {
+		document.getElementById('cbVaccination').checked = true;
+		document.getElementById('cbVaccination').disabled = true;
+	}
 }
